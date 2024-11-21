a0b32e69ba376dd68b91150c9e4ddb70dffbb38a
Naulet Théo <theo.naulet@gmail.com>
2024-11-21 15:59:48 +0100

    refactor(core): remove unused imports

--- src/main/java/com/example/rental_backend/config/SecurityConfig.java
+++ src/main/java/com/example/rental_backend/config/SecurityConfig.java
@@ -1,33 +1,6 @@
 package com.example.rental_backend.config;
 
-// import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
-
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
-
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfig {
-
-//     @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//         http
-//             .csrf(csrf -> csrf.disable()) // Désactiver CSRF
-//             .authorizeHttpRequests(auth -> auth
-//                 .requestMatchers("/api/auth/register", "/api/auth/login", "/api/rentals", "/api/rentals/*").permitAll() 
-//                 .anyRequest().authenticated() 
-//             );
-//         return http.build();
-//     }
-// }
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -35,7 +8,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
@@ -43,7 +15,6 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.rental_backend.service.CustomUserDetailsService;
