775a24dc36cefc0ed5a816d8aae28e6113db797f
Naulet Théo <theo.naulet@gmail.com>
2024-11-28 15:20:07 +0100

    refactor(user): remove unused import

--- src/main/java/com/example/rental_backend/controller/UserController.java
+++ src/main/java/com/example/rental_backend/controller/UserController.java
@@ -3,7 +3,6 @@ package com.example.rental_backend.controller;
 import com.example.rental_backend.dto.UserDTO;
 import com.example.rental_backend.service.UserService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
